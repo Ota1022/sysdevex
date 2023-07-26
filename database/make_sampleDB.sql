5d05459c5adbfcf785548a0b5c29057006560745
Itaru Ota <55938456+Ota1022@users.noreply.github.com>
2023-07-26 09:13:16 +0900

    ad  db samples

--- database/make_sampleDB.sql
+++ database/make_sampleDB.sql
@@ -112,18 +112,56 @@ INSERT INTO user_table(name, email, password) VALUES('admin', 'admin@test.ac.jp'
 /*user*/
 INSERT INTO user_table(name, email, password, address, tel, regist_date, birthday) VALUES('user1', 'user1@test.ac.jp', 'himitu', 'ìåãûìsêVèhãÊ', '03-1111-1111', '2023-06-20', '1999-01-01');
 INSERT INTO user_table(name, email, password, address, tel, regist_date, birthday) VALUES('user2', 'user2@test.ac.jp', 'himitu', 'ìåãûìsïiêÏãÊ', '03-1111-1111', '2023-07-19', '2000-01-01');
+INSERT INTO user_table(name, email, password, address, tel, regist_date, birthday) VALUES('user3', 'user3@test.ac.jp', 'himitu', 'ëÂç„ï{ëÂç„és', '06-2222-2222', '2023-08-01', '2001-02-02');
+INSERT INTO user_table(name, email, password, address, tel, regist_date, birthday) VALUES('user4', 'user4@test.ac.jp', 'himitu', 'ïüâ™åßïüâ™és', '092-3333-3333', '2023-08-02', '2002-03-03');
+INSERT INTO user_table(name, email, password, address, tel, regist_date, birthday) VALUES('user5', 'user5@test.ac.jp', 'himitu', 'ñkäCìπéDñyés', '011-4444-4444', '2023-08-03', '2003-04-04');
+INSERT INTO user_table(name, email, password, address, tel, regist_date, birthday) VALUES('user6', 'user6@test.ac.jp', 'himitu', 'à§ímåßñºå√âÆés', '052-5555-5555', '2023-08-04', '2004-05-05');
+INSERT INTO user_table(name, email, password, address, tel, regist_date, birthday) VALUES('user7', 'user7@test.ac.jp', 'himitu', 'ê_ìﬁêÏåßâ°ïlés', '045-6666-6666', '2023-08-05', '2005-06-06');
+INSERT INTO user_table(name, email, password, address, tel, regist_date, birthday) VALUES('user8', 'user8@test.ac.jp', 'himitu', 'çLìáåßçLìáés', '082-7777-7777', '2023-08-06', '2006-07-07');
+INSERT INTO user_table(name, email, password, address, tel, regist_date, birthday) VALUES('user9', 'user9@test.ac.jp', 'himitu', 'ãûìsï{ãûìsés', '075-8888-8888', '2023-08-07', '2007-08-08');
+INSERT INTO user_table(name, email, password, address, tel, regist_date, birthday) VALUES('user10', 'user10@test.ac.jp', 'himitu', 'ï∫å…åßê_åÀés', '078-9999-9999', '2023-08-08', '2008-09-09');
+INSERT INTO user_table(name, email, password, address, tel, regist_date, birthday) VALUES('user11', 'user11@test.ac.jp', 'himitu', 'ã{èÈåßêÂë‰és', '022-1010-1010', '2023-08-09', '2009-10-10');
+INSERT INTO user_table(name, email, password, address, tel, regist_date, birthday) VALUES('user12', 'user12@test.ac.jp', 'himitu', 'ê√â™åßê√â™és', '054-1111-1111', '2023-08-10', '2010-11-11');
+
 
 /*textbook*/
 INSERT INTO textbook(isbn, category_code, title, author) VALUES('0000000000000', 0, 'test book 0', 'test author 0');
 INSERT INTO textbook(isbn, category_code, title, author) VALUES('1111111111111', 1, 'test book 1', 'test author 1');
 INSERT INTO textbook(isbn, category_code, title, author) VALUES('2222222222222', 2, 'test book 2', 'test author 2');
+INSERT INTO textbook(isbn, category_code, title, author) VALUES('3333333333333', 3, 'test book 3', 'test author 3');
+INSERT INTO textbook(isbn, category_code, title, author) VALUES('4444444444444', 4, 'test book 4', 'test author 4');
+INSERT INTO textbook(isbn, category_code, title, author) VALUES('5555555555555', 5, 'test book 5', 'test author 5');
+INSERT INTO textbook(isbn, category_code, title, author) VALUES('6666666666666', 6, 'test book 6', 'test author 6');
+INSERT INTO textbook(isbn, category_code, title, author) VALUES('7777777777777', 7, 'test book 7', 'test author 7');
+INSERT INTO textbook(isbn, category_code, title, author) VALUES('8888888888888', 8, 'test book 8', 'test author 8');
+INSERT INTO textbook(isbn, category_code, title, author) VALUES('9999999999999', 9, 'test book 9', 'test author 9');
+
 
 /*inventory*/
 INSERT INTO inventory(isbn, user_id, state_code, price, isin_inventory_code, note, inventory_regist_date, inventory_update_date) VALUES('0000000000000', 2, 0, 500, 0, 'test (sold)', '2023-07-19', '2023-07-20');
 INSERT INTO inventory(isbn, user_id, state_code, price, isin_inventory_code, note, inventory_regist_date, inventory_update_date) VALUES('1111111111111', 2, 1, 1000, 1, 'test (not sold)', '2023-07-20', '2023-07-20');
+INSERT INTO inventory(isbn, user_id, state_code, price, isin_inventory_code, note, inventory_regist_date, inventory_update_date) VALUES('3333333333333', 3, 1, 500, 1, 'test (not sold)', '2023-08-01', '2023-08-01');
+INSERT INTO inventory(isbn, user_id, state_code, price, isin_inventory_code, note, inventory_regist_date, inventory_update_date) VALUES('4444444444444', 4, 2, 800, 0, 'test (sold)', '2023-08-02', '2023-08-02');
+INSERT INTO inventory(isbn, user_id, state_code, price, isin_inventory_code, note, inventory_regist_date, inventory_update_date) VALUES('5555555555555', 5, 3, 750, 1, 'test (not sold)', '2023-08-03', '2023-08-03');
+INSERT INTO inventory(isbn, user_id, state_code, price, isin_inventory_code, note, inventory_regist_date, inventory_update_date) VALUES('6666666666666', 6, 2, 650, 0, 'test (sold)', '2023-08-04', '2023-08-04');
+INSERT INTO inventory(isbn, user_id, state_code, price, isin_inventory_code, note, inventory_regist_date, inventory_update_date) VALUES('7777777777777', 7, 1, 900, 1, 'test (not sold)', '2023-08-05', '2023-08-05');
+INSERT INTO inventory(isbn, user_id, state_code, price, isin_inventory_code, note, inventory_regist_date, inventory_update_date) VALUES('8888888888888', 8, 2, 600, 0, 'test (sold)', '2023-08-06', '2023-08-06');
+INSERT INTO inventory(isbn, user_id, state_code, price, isin_inventory_code, note, inventory_regist_date, inventory_update_date) VALUES('9999999999999', 9, 3, 850, 1, 'test (not sold)', '2023-08-07', '2023-08-07');
+INSERT INTO inventory(isbn, user_id, state_code, price, isin_inventory_code, note, inventory_regist_date, inventory_update_date) VALUES('1010101010101', 10, 1, 700, 1, 'test (not sold)', '2023-08-08', '2023-08-08');
+INSERT INTO inventory(isbn, user_id, state_code, price, isin_inventory_code, note, inventory_regist_date, inventory_update_date) VALUES('1111111111112', 11, 2, 550, 0, 'test (sold)', '2023-08-09', '2023-08-09');
+
 
 /*sell*/
 INSERT INTO sell(user_id, inventory_id, sell_date) VALUES(1, 1, '2023-07-20');
+INSERT INTO sell(user_id, inventory_id, sell_date) VALUES(3, 2, '2023-08-02');
+INSERT INTO sell(user_id, inventory_id, sell_date) VALUES(4, 3, '2023-08-03');
+INSERT INTO sell(user_id, inventory_id, sell_date) VALUES(5, 4, '2023-08-04');
+INSERT INTO sell(user_id, inventory_id, sell_date) VALUES(6, 5, '2023-08-05');
+INSERT INTO sell(user_id, inventory_id, sell_date) VALUES(7, 6, '2023-08-06');
+INSERT INTO sell(user_id, inventory_id, sell_date) VALUES(8, 7, '2023-08-07');
+INSERT INTO sell(user_id, inventory_id, sell_date) VALUES(9, 8, '2023-08-08');
+INSERT INTO sell(user_id, inventory_id, sell_date) VALUES(10, 9, '2023-08-09');
+INSERT INTO sell(user_id, inventory_id, sell_date) VALUES(11, 10, '2023-08-10');
 
 ALTER TABLE category OWNER TO student;
 ALTER TABLE state OWNER TO student;
